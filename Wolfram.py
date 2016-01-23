f7131328a495169bb902847ee802775ab59b2abc
kevinjqliu <kevin.jq.liu@gmail.com>
2016-01-23 14:00:40 -0500

    Parsed XML tree

--- Wolfram.py
+++ Wolfram.py
@@ -1,8 +1,20 @@
 import wolframalpha
-import bs4
+import xml.etree.ElementTree as ET
+
+# Open Wolfram API ID from local file
+def wolframapi():
+    fileName = 'WolframAPI.txt'
+    f = open(fileName, 'r')
+
+    APP_ID = f.readline()
+    return APP_ID
 
 def main():
-    client = wolframalpha.Client(APP_ID)
+    # get the Wolfram API
+    APP_ID = wolframapi()
+
+    # Connect to Wolfram
+    client = wolframalpha.Client(APP_ID[:-1])
 
     query = raw_input("Enter converstion topic: ")
 
@@ -12,10 +24,14 @@ def main():
     print res
 
     print "results: "
-    print res.results
+
+    for r in res.results:
+        print r
 
     print "tree: "
-    print res.tree
+    #print  ET.tostring(res.tree.getroot())
+
+    print res.tree.find('examplepage').get('category')
 
     print "pods: "
     print res.pods

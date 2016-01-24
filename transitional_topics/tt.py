6615f782d358d5e5a75602ebbde1fad41bfd91fd
Ty Coghlan <Ophirr33@zimbra.ccs.neu.edu>
2016-01-23 20:15:59 -0500

    Fully parsed the thesaurus

--- transitional_topics/tt.py
+++ transitional_topics/tt.py
@@ -1,18 +1,24 @@
 import json
 import string
 import time
+import wikipedia
 import rosette.api
 
-from rosette.api import API, DocumentParameters, MorphologyOutput
+from rosette.api import API, DocumentParameters, MorphologyOutput, NameMatchingParameters
 
-def run(input, key="4a2ce33afde2071fd73b8c8f3977e4ea", altUrl='https://api.rosette.com/rest/v1/'):
+user_key = "7633a1fc4dab2a460ec878d2f42b1972"
+ros_url = 'https://api.rosette.com/rest/v1/'
+
+def run(input, key=user_key, altUrl=ros_url):
     api = API(user_key=key, service_url=altUrl)
     content = ''.join(ch.lower() for ch in input if ch not in set(string.punctuation))
     params = DocumentParameters()
     params["content"] = content
+    params["language"] = "eng"
     return api.morphology(params, MorphologyOutput.LEMMAS)
 
-# conversta a list of strings into a list of its lemmas
+
+# converts a a list of strings into a list of its lemmas
 def lemma_list(lst):
     result = run(str(lst))
     json_obj = json.loads(json.dumps(result, indent=2, ensure_ascii=False))
@@ -20,17 +26,82 @@ def lemma_list(lst):
     lst.sort()
     return lst
 
+
 def convert_file():
     f = open('mthes/mobythes.aur', 'r')
-    f = open('out.txt')
+    f2 = open('out2.txt', 'w')
     line = f.readline()
+    i = 1
     while line != "":
         time.sleep(.1)
         line = line.split(',')
         key = lemma_list(line[0:1])
         rest = lemma_list(line[1:])
         key.extend(rest)
-        print(str(key))
+        f2.write(",".join(key) + "\n")
+        print(i)
+        i+=1
         line = f.readline()
     f.close()
+    f2.close()
+
 convert_file()
+
+def find_similar(key_word, key=user_key, altUrl=ros_url):
+    api = API(user_key=key, service_url=altUrl)
+    if len(key_word.split()) > 1:
+        key_word = "_".join(key_word.split())
+    params = DocumentParameters()
+    params["content"] = key_word
+    params["language"] = "eng"
+    json_obj = json.loads(json.dumps(api.morphology(params, MorphologyOutput.LEMMAS),
+                                   indent=2, ensure_ascii=False))
+    return search_for(json_obj['lemmas'][0]['lemma'])
+
+#print(find_similar("Bear down"))
+
+
+def search_for(name):
+    f = open('out.txt', 'r')
+    line = f.readline()
+    while not (line == "" or line.split(",")[0] == name):
+        line = f.readline()
+    f.close()
+    if line == "":
+        f.close()
+        f = open('out.txt', 'r')
+        line = f.readline()
+        while not (line == "" or name in line):
+            line = f.readline()
+
+        if line == "":
+            "No keyword found"
+        else:
+            line.replace(name, "")
+    else:
+        line.replace(name, "");
+        return line
+
+#print(find_similar("deep_sea"))
+
+def proper_noun(key_word, key=user_key, altUrl=ros_url):
+     api = API(user_key=key, service_url=altUrl)
+     params = DocumentParameters()
+     params["content"] = key_word
+     params["language"] = "eng"
+     api = api.morphology(params, MorphologyOutput.PARTS_OF_SPEECH)
+     return "PROP" in str(json.loads(json.dumps(api, indent=2, ensure_ascii=False)))
+
+#print(proper_noun("Jianqial Liu"))
+
+def proper_key_words(key_word, key=user_key, altUrl=ros_url):
+    # Create an API instance
+    api = API(user_key=key, service_url=altUrl)
+
+    params = NameMatchingParameters()
+    i = 1
+    for x in wikipedia.search(key_word):
+        params["name" + str(i)] = {"text": x, "language": "eng"}
+    return json.loads(json.dumps(api.matched_name(params), indent=2, ensure_ascii=False))
+
+#print(str(proper_key_words("Barrack Obama")))

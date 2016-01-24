97eced851205564f18d191c9fcf341a347cdcbda
Ty Coghlan <Ophirr33@zimbra.ccs.neu.edu>
2016-01-23 22:25:46 -0500

    Added main 'look_up' function'

--- transitional_topics/tt.py
+++ transitional_topics/tt.py
@@ -4,14 +4,16 @@ import time
 import wikipedia
 import heapq
 import api_key
+import random
 
 from rosette.api import API, DocumentParameters, MorphologyOutput, NameMatchingParameters
 
 user_key = api_key.user_key
 ros_url = api_key.ros_url
 
-def run(input, key=user_key, altUrl=ros_url):
-    api = API(user_key=key, service_url=altUrl)
+
+def run(input, key=user_key, alt_url=ros_url):
+    api = API(user_key=key, service_url=alt_url)
     content = ''.join(ch.lower() for ch in input if ch not in set(string.punctuation))
     params = DocumentParameters()
     params["content"] = content
@@ -28,6 +30,7 @@ def lemma_list(lst):
     return lst
 
 
+# for converting thesaurus, does not need to be run
 def convert_file():
     f = open('mthes/mobythes.aur', 'r')
     f2 = open('out2.txt', 'w')
@@ -41,26 +44,40 @@ def convert_file():
         key.extend(rest)
         f2.write(",".join(key) + "\n")
         print(i)
-        i+=1
+        i += 1
         line = f.readline()
     f.close()
     f2.close()
 
-#convert_file()
 
-def find_similar(key_word, key=user_key, altUrl=ros_url):
-    api = API(user_key=key, service_url=altUrl)
+# convert_file()
+
+def find_similar(key_word, key=user_key, alt_url=ros_url):
+    api = API(user_key=key, service_url=alt_url)
     if len(key_word.split()) > 1:
         key_word = "_".join(key_word.split())
     params = DocumentParameters()
     params["content"] = key_word
     params["language"] = "eng"
     json_obj = json.loads(json.dumps(api.morphology(params, MorphologyOutput.LEMMAS),
-                                   indent=2, ensure_ascii=False))
+                                     indent=2, ensure_ascii=False))
     return search_for(json_obj['lemmas'][0]['lemma'])
 
 
-def search_for(name: string):
+def parse_with_queue(json_obj, key_word):
+    queue = []
+    for x in json_obj['entities']:
+        if x['normalized'] in key_word:
+            continue
+        confidence = int(x['confidence'])
+        if len(queue) < 4:
+            heapq.heappush(queue, (confidence, x['normalized']))
+        elif queue[0][0] > confidence:
+            heapq.heappushpop(queue, (confidence, x['normalized']))
+    return [x[1] for x in queue]
+
+
+def search_for(name: string, key=user_key, alt_url=ros_url):
     f = open('out.txt', 'r')
     line = f.readline()
     while not (line == "" or line.split(",")[0] == name):
@@ -77,27 +94,32 @@ def search_for(name: string):
         else:
             line.replace(name, "")
             f.close()
-            return line
     else:
         f.close()
-        line.replace(name, "");
-        return line
+        line.replace(name, "")
+    line = line.split(",");
+    random.shuffle(line)
+    return line[:4]
 
-print(find_similar("deepsea"))
 
-def proper_noun(key_word, key=user_key, altUrl=ros_url):
-     api = API(user_key=key, service_url=altUrl)
-     params = DocumentParameters()
-     params["content"] = key_word
-     params["language"] = "eng"
-     api = api.morphology(params, MorphologyOutput.PARTS_OF_SPEECH)
-     return "PROP" in str(json.loads(json.dumps(api, indent=2, ensure_ascii=False)))
+#print(find_similar("deepsea"))
 
-#print(proper_noun("Jianqial Liu"))
 
-def proper_key_words(key_word, key=user_key, altUrl=ros_url):
+def proper_noun(key_word, key=user_key, alt_url=ros_url):
+    api = API(user_key=key, service_url=alt_url)
+    params = DocumentParameters()
+    params["content"] = key_word
+    params["language"] = "eng"
+    api = api.morphology(params, MorphologyOutput.PARTS_OF_SPEECH)
+    return "PROP" in str(json.loads(json.dumps(api, indent=2, ensure_ascii=False)))
+
+
+# print(proper_noun("Jianqial Liu"))
+# ->True
+
+def proper_key_words(key_word, key=user_key, alt_url=ros_url):
     # Create an API instance
-    api = API(user_key=key, service_url=altUrl)
+    api = API(user_key=key, service_url=alt_url)
 
     params = NameMatchingParameters()
     params['name1'] = key_word
@@ -111,17 +133,15 @@ def proper_key_words(key_word, key=user_key, altUrl=ros_url):
     params = DocumentParameters()
     params['content'] = wikipedia.summary(max[0])
     json_obj = json.loads(json.dumps(api.entities(params), indent=2, ensure_ascii=False,
-                                 sort_keys=True))
-    queue = []
-    for x in json_obj['entities']:
-        if x['normalized'] in key_word:
-            continue
-        confidence = int(x['confidence'])
-        if len(queue) < 4:
-            heapq.heappush(queue, (confidence, x['normalized']))
-        elif queue[0][0] > confidence:
-            heapq.heappushpop(queue, (confidence, x['normalized']))
-    return [x[1] for x in queue]
+                                     sort_keys=True))
+    return parse_with_queue(json_obj, key_word)
 
 
-#print(str(proper_key_words("Barrack Obama")))
+    # print(str(proper_key_words("Barrack Obama")))
+    # ->['president', 'U.S.', "American', 'United States']
+
+def look_up(key_word, key=user_key, alt_url=ros_url):
+    if proper_noun(key_word):
+        return proper_key_words(key_word);
+    else:
+        return find_similar(key_word)

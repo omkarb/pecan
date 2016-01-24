261ec323421e15b0fe5917911b9851610693cee9
Ty Coghlan <Ophirr33@zimbra.ccs.neu.edu>
2016-01-24 09:34:47 -0500

    Fixed wiki error in snapsis

--- synopsis.py
+++ synopsis.py
@@ -1,6 +1,9 @@
 import wikipedia
-
-
+import api_key
+import json
+from rosette.api import API, NameMatchingParameters
+user_key = api_key.user_key
+ros_url = api_key.ros_url
 
 # Search topic query
 def search_topic():
@@ -9,8 +12,23 @@ def search_topic():
 
 
 # Grab summary from wikipedia
-def wikipedia_summary(query):
-    return wikipedia.summary(query)
+def wikipedia_summary(query, key=user_key, alt_url=ros_url):
+    # Create an API instance
+    api = API(user_key=key, service_url=alt_url)
+
+    params = NameMatchingParameters()
+    params['name1'] = key_word
+    max = ['error', 0]
+    for x in wikipedia.search(key_word):
+        params["name2"] = x
+        val = json.loads(json.dumps(api.matched_name(params), indent=2,
+                                    ensure_ascii=False))['result']['score'] > max[1]
+        if val > max[1]:
+            max = [x, val]
+    if max[0] != 'error':
+        return wikipedia.summary(max[0])
+    else:
+        raise Exception("No wiki articles found")
 
 
 def main():
